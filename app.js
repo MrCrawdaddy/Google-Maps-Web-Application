a7c81d5d684b16826fe5def3ec61143d2414ffac
ben <crawfordbt@appstate.edu>
2016-04-29 22:58:46 -0400

    commit message

--- app.js
+++ app.js
@@ -1,60 +1,37 @@
+// Dependencies
+// -----------------------------------------------------
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
+var port = process.env.PORT || 3000;
+var morgan = require('morgan');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var methodOverride = require('method-override');
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Express Configuration
+// -----------------------------------------------------
+// Sets the connection to MongoDB
+mongoose.connect("mongodb://localhost/MeanMapApp");
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Logging and Parsing
+app.use(express.static(__dirname + '/public')); // sets the static files location to public
+app.use('/bower_components', express.static(__dirname + '/bower_components')); // Use BowerComponents
+app.use(morgan('dev')); // log with Morgan
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.text()); // allows bodyParser to look at raw text
+app.use(bodyParser.json({
+  type: 'application/vnd.api+json'
+})); // parse application/vnd.api+json as json
+app.use(methodOverride());
 
-app.use('/', routes);
-app.use('/users', users);
+// Routes
+// ------------------------------------------------------
+require('./routes/routes.js')(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+// Listen
+// -------------------------------------------------------
+app.listen(port);
+console.log('App listening on port ' + port);
